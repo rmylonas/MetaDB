4a83782428eb7794286957b09f4913cc756ad141
rmylonas <roman.mylonas@gmail.com>
2014-04-07 11:27:06 +0000

    create ZIP file using Ant

--- src/groovy/it/fmach/metadb/workflow/metams/MetaMsZipExporter.groovy
+++ src/groovy/it/fmach/metadb/workflow/metams/MetaMsZipExporter.groovy
@@ -2,6 +2,7 @@ package it.fmach.metadb.workflow.metams
 
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
+import groovy.util.AntBuilder
 
 class MetaMsZipExporter {
 	
@@ -10,22 +11,10 @@ class MetaMsZipExporter {
 		tempFile.delete()
 		
 		String zipFileName = tempFile.absolutePath
-		String inputDir = resultDir
-		  
-		ZipOutputStream zipFile = new ZipOutputStream(new FileOutputStream(zipFileName))
-		new File(inputDir).eachFile() { file ->
-			zipFile.putNextEntry(new ZipEntry(file.getName()))
-			def buffer = new byte[1024]
-			file.withInputStream { i ->
-				def l = i.read(buffer)
-				// check wether the file is empty
-				if (l > 0) {
-					zipFile.write(buffer, 0, l)
-				}
-			}
-			zipFile.closeEntry()
-		}
-		zipFile.close()
+		
+		// create zip file using Ant
+		def ant = new AntBuilder()
+		ant.zip(destfile: zipFileName, basedir: resultDir)
 		
 		return zipFileName
 	}	
